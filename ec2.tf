6e42bdd311dd04a57be672d6e61abb636d30a75d
Kenechukwu Chikwendu <kenechikwendu@gmail.com>
2023-02-15 22:44:39 -0400

    adding ebs and root_block_device

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   monitoring = true
-  ebs_optimized = 
+  ebs_optimized = true
   root_block_device {
     encrypted = true
   }
