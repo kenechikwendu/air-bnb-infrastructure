90a09b7752b5833d91caa549f83ba064385c4a62
Kenechukwu Chikwendu <kenechikwendu@gmail.com>
2023-02-17 06:36:13 -0400

    correcting iam instance profile

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  iam_instance_profile = "ansible-dynamic-inventory-role"
+  iam_instance_profile = "test"
   monitoring = true
   ebs_optimized = true
   root_block_device {
