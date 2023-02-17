d69b06f897995681c934be4b0c589901bc3fb866
Kenechukwu Chikwendu <kenechikwendu@gmail.com>
2023-02-17 10:33:44 -0400

    correcting iam instance profile

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,16 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  
+  iam_instance_profile = "test"
+
+  monitoring = true
+
+  ebs_optimized = true
+
+  root_block_device {
+      encrypted = true
+  }
+
   
   tags = {
     Name = var.tags
